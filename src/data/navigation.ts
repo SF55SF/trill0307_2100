ea1cfff7c0495548094e5ebc319e6e0b654057df
SF55SF <2712055@gmail.com>
2026-07-03 20:58:35 +0500

    Add files via upload

---
+++ src/data/navigation.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+  { label: 'О центре', href: '#about' },
+  { label: 'Офисы', href: '#offices' },
+  { label: 'Галерея', href: '#interiors' },
+  { label: 'Инфраструктура', href: '#infrastructure' },
+  { label: 'Расположение', href: '#location' },
+  { label: 'Контакты', href: '#contacts' }
+];
